4bed209c6aeba3c16fc7001dbf633ada7c116232
Николай Митин <nick.mitin@gmail.com>
2020-04-12 10:33:12 +0300

    Добавили методы создания и обновления данных в сервис

--- src/Services/GenericService.php
+++ src/Services/GenericService.php
@@ -2,9 +2,18 @@
 
 namespace Qortex\Bootstrap\Services;
 
+use Illuminate\Database\Eloquent\Model;
+
 abstract class GenericService
 {
 	protected $modelClassName = null;
+
+	protected $excludeProperties = [];
+
+	private $defaultExcludeProperties = [
+		'created_at', 
+		'updated_at'
+	];
 	
 	protected function getByFieldValueSingle($name, $value) {
 		return $this->modelClassName::where($name, $value)->first();
@@ -29,6 +38,29 @@ abstract class GenericService
 	public function getByIds($ids) {
 		$keyFieldName = (new $this->modelClassName())->getKeyName();
 		return $this->getByFieldValueIn($keyFieldName, $ids);
+	}
+
+	private function setProperties(Model $model, array $properties): void
+	{
+		$excludeProperties = array_merge($this->defaultExcludeProperties, $this->excludeProperties);
+		foreach ($properties as $name => $value) {
+			if (!in_array($name, $excludeProperties)) {
+				$model->$name = $value;
+			}
+		}
+	}
+
+	public function create(array $properties): Model
+	{
+		$model = new $this->modelClassName;
+		return $this->update($model, $properties);
+	}
+
+	public function update(Model $model, array $properties): Model
+	{
+		$this->setProperties($model, $properties);
+		$model->save();
+		return $model;
 	}
 	
 }
